c37c67d8e2ba6b54442bf8d3693eba759e162d0e
timelyart <timelyart@protonmail.com>
2018-11-05 11:02:09 +0100

    Fixed setup mess

--- setup.py
+++ setup.py
@@ -6,10 +6,12 @@
     If you want to create version 2 going from version 1.1.4 create tag 1
 """
 from setuptools import setup, find_packages
+from pkg_resources import get_distribution
 from os import path
+release = get_distribution('kairos').version
 this_directory = path.abspath(path.dirname(__file__))
-# with open(path.join(this_directory, 'README.md')) as f:
-#     long_description = f.read()
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
 
 setup(
     name='Kairos',
@@ -17,14 +19,9 @@ setup(
     url='https://github.com/timelyart/Kairos/',
     license='GNU General Public License v3.0',
     author='timelyart',
+    version=release,
     author_email='timelyart@protonmail.com',
     description='Kairos aims to help you save time by automating repetitive tasks on TradingView such as refreshing alerts and creating new ones.',
-    # long_description=long_description,
-    long_description_content_type='text/markdown',
-    use_scm_version={
-        'write_to': 'version.txt',
-        'tag_regex': r'^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$',
-    },
-    # setup_requires=['pip>=18.1', 'setuptools_scm'],
-    install_requires=['pyyaml', 'beautifulsoup4', 'urllib3', 'selenium', 'configparser', 'tools', 'pip>=18.1', 'setuptools_scm'],
+    long_description=long_description,
+    install_requires=['pyyaml', 'beautifulsoup4', 'urllib3', 'selenium', 'configparser', 'tools', 'pip>=18.1'],
 )
