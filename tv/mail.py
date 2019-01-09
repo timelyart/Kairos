d0c4f23eb97d1ad14855e17851f7ed0d89ad14f0
timelyart <timelyart@protonmail.com>
2019-01-09 13:06:10 +0100

    Fix: apparently json.dump creates a unicode string which gives problems with webhooks

--- tv/mail.py
+++ tv/mail.py
@@ -495,14 +495,15 @@ def send_to_webhooks(data, webhooks, search_criteria='', batch_size=0):
         batches.append(batch)
 
         for i in range(len(batches)):
-            json_string = json.dumps({'signals': batches[i]})
 
             for j in range(len(webhooks)):
                 if webhooks[j]:
-                    log.debug('webhook: ' + str(webhooks[j]))
-                    log.info('json: ' + str(json_string))
+                    json_data = {'signals': batches[i]}
+                    log.debug(repr(json_data))
                     # result = [200, 'OK']
-                    r = requests.post(str(webhooks[j]), json=json_string)
+                    # r = requests.post(str(webhooks[j]), json=json_string)
+                    r = requests.post(str(webhooks[j]), json=json_data)
+
                     # unfortunately, we cannot always send a raw image (e.g. zapier)
                     # elif filename:
                     #     screenshot_bytestream = ''
@@ -513,6 +514,7 @@ def send_to_webhooks(data, webhooks, search_criteria='', batch_size=0):
                     #     except Exception as send_webhook_error:
                     #         log.exception(send_webhook_error)
                     #     r = requests.post(webhook_url, json={'date': date, 'symbol': symbol, 'alert': alert, 'chart_url': url, 'screenshot_url': screenshot, 'screenshot_bytestream': screenshot_bytestream})
+
                     result = [r.status_code, r.reason]
                     if result[0] != 200:
                         log.warn(str(webhooks[j]) + ' ' + str(i+1) + '/' + str(len(batches)) + ' ' + str(result[0]) + ' ' + str(result[1]))
