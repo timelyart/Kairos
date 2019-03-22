cd195929d6eb5393be7ac63a22d73f5325a4f110
timelyart <timelyart@protonmail.com>
2019-03-22 11:17:23 +0100

    Added wrapper for logging and moved core code to debug.py Removed Switch (wasn't used) Added to_csv, get_timezone, get_time_offset, dt_parse, remove_empty_lines

--- kairos/tools.py
+++ kairos/tools.py
@@ -1,13 +1,22 @@
 # File: tools.py
 import os
+from datetime import datetime, timedelta
+import time
+
 from kairos import debug
 from collections import OrderedDict
 from configparser import RawConfigParser
 
-log = debug.create_log()
+
+def create_log():
+    return debug.create_log()
 
 
-def get_config(current_dir):
+def write_console_log(browser, clear_on_startup=True):
+    return debug.write_console_log(browser, clear_on_startup)
+
+
+def get_config(current_dir, log):
     config = RawConfigParser(allow_no_value=True, strict=False, empty_lines_in_values=False, dict_type=ConfigParserMultiValues, converters={"list": ConfigParserMultiValues.getlist})
     config_file = os.path.join(current_dir, "kairos.cfg")
     if os.path.exists(config_file):
@@ -16,7 +25,7 @@ def get_config(current_dir):
             debug.clear_log()
         log.setLevel(config.getint('logging', 'level'))
     else:
-        log.error("File " + config_file + " does not exist")
+        log.error("File {} does not exist".format(config_file))
         log.exception(FileNotFoundError)
         exit(0)
     return config
@@ -44,12 +53,55 @@ class ConfigParserMultiValues(OrderedDict):
         return result
 
 
-class Switch:
-    def __init__(self, value): self._val = value
+def to_csv(log, data, delimeter=','):
+    result = ''
+    log.info(str(type(data)) + ': ' + str(data))
+    if isinstance(data, dict):
+        for _key in data:
+            if result == '':
+                result = to_csv(log, data[_key])
+            else:
+                result = delimeter + to_csv(log, data[_key], delimeter)
+    elif isinstance(data, list):
+        for item in data:
+            if result == '':
+                result = to_csv(log, item)
+            else:
+                result = delimeter + to_csv(log, item, delimeter)
+    else:
+        result = data
+    return result
 
-    def __enter__(self): return self
 
-    # Allows traceback to occur
-    def __exit__(self, type, value, traceback): return False
+def get_timezone():
+    timestamp = time.time()
+    date_utc = datetime.utcfromtimestamp(timestamp)
+    date_local = datetime.fromtimestamp(timestamp)
+    date_delta = max(date_utc, date_local) - min(date_utc, date_local)
+    timezone = '{0:0{width}}'.format(int(date_delta.seconds / 60 / 60), width=2) + '00'
+    if date_local > date_utc:
+        timezone = '+' + timezone
+    elif date_local < date_utc:
+        timezone = '-' + timezone
+    return timezone
 
-    def __call__(self, *mconds): return self._val in mconds
+
+def get_time_offset():
+    timestamp = time.time()
+    date_utc = datetime.utcfromtimestamp(timestamp)
+    date_local = datetime.fromtimestamp(timestamp)
+    date_delta = date_local - date_utc
+    return date_delta
+
+
+def dt_parse(t):
+    ret = datetime.strptime(t[0:16], '%Y-%m-%dT%H:%M')
+    if t[17] == '+':
+        ret -= timedelta(hours=int(t[18:20]), minutes=int(t[20:]))
+    elif t[17] == '-':
+        ret += timedelta(hours=int(t[18:20]), minutes=int(t[20:]))
+    return ret
+
+
+def remove_empty_lines(text):
+    return "".join([s for s in text.splitlines(True) if s.strip("\r\n")])
