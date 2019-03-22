b8b6b5d0b6034adfceb501fbe0309e4c87083086
timelyart <timelyart@protonmail.com>
2019-03-22 11:19:15 +0100

    Added methods to write browser logs which is usually one but can be up to five

--- kairos/debug.py
+++ kairos/debug.py
@@ -1,8 +1,9 @@
 # File: debug.py
 import logging
 import sys
+from selenium.common.exceptions import InvalidArgumentException, WebDriverException
 
-log_path = '.'  # project dir
+log_path = './log'  # project dir
 file_name = 'debug.log'
 
 
@@ -22,3 +23,49 @@ def create_log():
             logging.StreamHandler(sys.stdout)
         ])
     return logging.getLogger()
+
+
+def load_console_log(browser, log_type):
+    try:
+        return browser.get_log(log_type)
+    except InvalidArgumentException:
+        return None
+    except WebDriverException:
+        return None
+
+
+def write_console_log(browser, clear_on_startup=True):
+    import os
+    from kairos import tools
+    from datetime import datetime
+    from datetime import timedelta
+
+    logs = {
+        'browser': load_console_log(browser, 'browser'),
+        'driver': load_console_log(browser, 'driver'),
+        'server': load_console_log(browser, 'server'),
+        'client': load_console_log(browser, 'client'),
+        'performance': load_console_log(browser, 'performance')
+    }
+
+    if not os.path.exists(log_path):
+        os.mkdir(log_path)
+
+    offset = tools.get_time_offset()
+    for log_name in logs:
+        if logs[log_name]:
+            file = os.path.join(r"" + log_path, str(log_name) + ".log")
+            if clear_on_startup:
+                with open(file, 'w'):
+                    pass
+            f = open(file, 'a')
+            lines = logs[log_name]
+            for line in lines:
+                level = line['level']
+                message = line['message']
+                timestamp = datetime(1970, 1, 1, ) + timedelta(microseconds=line['timestamp']*1000) + offset
+                s_timestamp = timestamp.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+                # apparently, the webdriver adds a carriage return ('\r') after each entry but no line feed ('\n')
+                output = "{} {} \t {} \n".format(s_timestamp, level, message)
+                f.write(output)
+            f.close()
