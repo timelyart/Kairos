44db579d4a445a63dc8c9a525623d9be9ddef694
timelyart <timelyart@protonmail.com>
2019-04-26 11:23:07 +0200

    Fix: urllib3 version conflict

--- setup.py
+++ setup.py
@@ -18,11 +18,11 @@ setup(
     url='https://github.com/timelyart/Kairos/',
     license='GNU General Public License v3.0',
     author='timelyart',
-    version='1.2.9',
+    version='1.2.10',
     author_email='timelyart@protonmail.com',
     description='Kairos aims to help you save time by automating repetitive tasks on TradingView such as refreshing alerts and creating new ones.',
     long_description=long_description,
-    install_requires=['pyyaml', 'beautifulsoup4', 'urllib3', 'selenium', 'configparser', 'tools', 'pip>=18.1', 'Pillow', 'requests>=2.21', 'gspread', 'google-api-python-client', 'oauth2client'],
+    install_requires=['pyyaml', 'beautifulsoup4', 'urllib3>=1.21.1,<1.25', 'selenium', 'configparser', 'tools', 'pip>=18.1', 'Pillow', 'requests>=2.21', 'gspread', 'google-api-python-client', 'oauth2client'],
     extras_require={
         'platform_system == "Windows"': [],
         'platform_system == "Linux"': ['python3-Xlib'],
