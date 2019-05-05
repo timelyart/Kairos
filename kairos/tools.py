93f0a3f83567939d266884607bb16c3b6389be1b
timelyart <timelyart@protonmail.com>
2019-05-06 01:45:29 +0200

    Added method display_time
    Signed-off-by: timelyart <timelyart@protonmail.com>

--- kairos/tools.py
+++ kairos/tools.py
@@ -178,3 +178,25 @@ def get_yaml_config(file, log, root=False):
     except OSError as err_os:
         log.exception(err_os)
     return result
+
+
+time_intervals = (
+    ('weeks', 604800),  # 60 * 60 * 24 * 7
+    ('days', 86400),    # 60 * 60 * 24
+    ('hours', 3600),    # 60 * 60
+    ('minutes', 60),
+    ('seconds', 1),
+    )
+
+
+def display_time(seconds, granularity=2):
+    result = []
+
+    for name, count in time_intervals:
+        value = seconds // count
+        if value:
+            seconds -= value * count
+            if value == 1:
+                name = name.rstrip('s')
+            result.append("{} {}".format(int(value), name))
+    return ', '.join(result[:granularity])
