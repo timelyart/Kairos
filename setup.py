4d68182bcd1a34051f3b336bfcce815f3614e821
timelyart <timelyart@protonmail.com>
2020-01-28 14:18:15 +0100

    Added Cython to the required packages
    Signed-off-by: timelyart <timelyart@protonmail.com>

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     description='Kairos aims to help you save time by automating repetitive tasks on TradingView such as refreshing alerts and creating new ones.',
     long_description=long_description,
     install_requires=['pyyaml', 'beautifulsoup4', 'urllib3>=1.21.1,<1.25', 'selenium', 'configparser', 'tools', 'pip>=18.1', 'Pillow', 'requests>=2.21', 'gspread', 'google-api-python-client', 'oauth2client', 'pymongo', 'dill', 'numpy',
-                      'fastnumbers', 'psutil'],
+                      'fastnumbers', 'psutil', 'Cython'],
     extras_require={
         'platform_system == "Windows"': [],
         'platform_system == "Linux"': ['python3-Xlib'],
