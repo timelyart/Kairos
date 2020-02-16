23892e4435928fef5659e913106be57f17f30bbd
timelyart <timelyart@protonmail.com>
2020-02-16 18:31:50 +0100

    Updated requirements
    Signed-off-by: timelyart <timelyart@protonmail.com>

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ with open(path.join(this_directory, 'README.md')) as f:
 setup(
     name='Kairos',
     packages=find_packages(),
-    license='End-User License Agreement: https://eulatemplate.com/live.php?token=F2am7Ud98HlFDECoTq2GYhIksQmn6T9A',
-    author='Sanne Appel',
+    license='End-User License Agreement: https://www.eulatemplate.com/live.php?token=zduUYsiBp0PFzzX0UANHUodYVMifuNRp',
+    author='timelyart',
     version='2.3',
-    author_email='sjappel@protonmail.com',
+    author_email='timelyart@protonmail.com',
     description='Kairos aims to help you save time by automating repetitive tasks on TradingView such as refreshing alerts and creating new ones.',
     long_description=long_description,
     install_requires=['pyyaml', 'beautifulsoup4', 'urllib3>=1.21.1,<1.25', 'selenium', 'configparser', 'tools', 'pip>=18.1', 'Pillow', 'requests>=2.21', 'gspread', 'google-api-python-client', 'oauth2client', 'pymongo', 'dill', 'numpy',
