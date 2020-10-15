d61d490ffdf225ca54df40b77e958edc3c0cb464
timelyart <timelyart@protonmail.com>
2020-10-15 14:04:40 +0200

    Set selenium version to 3.141 until version 4 is properly vetted and Kairos has been migrated to it

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ setup(
     packages=find_packages(),
     license='End-User License Agreement: https://www.eulatemplate.com/live.php?token=zduUYsiBp0PFzzX0UANHUodYVMifuNRp',
     author='timelyart',
-    version='2.6',
+    version='2.7',
     author_email='timelyart@protonmail.com',
     description='Kairos aims to help you save time by automating repetitive tasks on TradingView such as refreshing alerts and creating new ones.',
     long_description=long_description,
-    install_requires=['pyyaml', 'beautifulsoup4', 'urllib3', 'selenium', 'configparser', 'tools', 'pip', 'Pillow', 'requests', 'gspread', 'google-api-python-client', 'oauth2client', 'pymongo', 'dill', 'numpy',
+    install_requires=['pyyaml', 'beautifulsoup4', 'urllib3', 'selenium>=3.141,<4', 'configparser', 'tools', 'pip', 'Pillow', 'requests', 'gspread', 'google-api-python-client', 'oauth2client', 'pymongo', 'dill', 'numpy',
                       'fastnumbers', 'psutil', 'Cython'],
     extras_require={
         'platform_system == "Windows"': [],
