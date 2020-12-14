3e1891f0a1f4465a624b455e337c9a040fde1fa5
timelyart <timelyart@protonmail.com>
2020-12-14 18:28:23 +0100

    Set numpy dependency to version 1.19.4 in order to maintain support for python 3.6

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ setup(
     packages=find_packages(),
     license='End-User License Agreement: https://www.eulatemplate.com/live.php?token=zduUYsiBp0PFzzX0UANHUodYVMifuNRp',
     author='timelyart',
-    version='2.10',
+    version='2.11',
     author_email='timelyart@protonmail.com',
     description='Kairos aims to help you save time by automating repetitive tasks on TradingView such as refreshing '
                 'alerts and creating new ones.',
     long_description=long_description,
     install_requires=['pyyaml', 'beautifulsoup4', 'urllib3', 'selenium>=3.141,<4', 'configparser', 'pip', 'Pillow',
-                      'requests', 'gspread', 'google-api-python-client', 'oauth2client', 'pymongo', 'dill', 'numpy',
+                      'requests', 'gspread', 'google-api-python-client', 'oauth2client', 'pymongo', 'dill', 'numpy==1.19.4',
                       'fastnumbers', 'psutil', 'Cython', 'tqdm'],
     extras_require={
         'platform_system == "Windows"': [],
