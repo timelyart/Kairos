8250fabf22ccc2254442d82b840ed910ad77bf9d
timelyart <timelyart@protonmail.com>
2021-07-30 01:51:29 +0200

    Added soupsieve to requirements

--- cython.py
+++ cython.py
@@ -51,8 +51,9 @@ setup(
     author_email='timelyart@protonmail.com',
     description='Kairos aims to help you save time by automating repetitive tasks on TradingView such as refreshing alerts and creating new ones.',
     long_description=long_description,
-    install_requires=['pyyaml', 'beautifulsoup4', 'urllib3', 'selenium', 'configparser', 'tools', 'pip>=18.1', 'Pillow', 'requests>=2.21', 'gspread', 'google-api-python-client', 'oauth2client', 'pymongo', 'dill', 'numpy',
-                      'fastnumbers', 'psutil'],
+    install_requires=['pyyaml', 'beautifulsoup4', 'urllib3>=1.26.5', 'selenium>=3.141,<4', 'configparser', 'tools', 'pip',
+                      'Pillow', 'requests', 'gspread', 'google-api-python-client', 'oauth2client', 'pymongo', 'dill', 'numpy',
+                      'fastnumbers', 'psutil', 'tqdm', 'soupsieve'],
     extras_require={
         'platform_system == "Windows"': [],
         'platform_system == "Linux"': ['python3-Xlib'],
