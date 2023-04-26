70a86707f494534db815ef78c9cba151a0495ae1
timelyart <timelyart@protonmail.com>
2023-04-26 15:41:41 +0200

    Changed dependency management via setuptools from setup.py to setup.cfg per setuptools recommendations
    Signed-off-by: timelyart <timelyart@protonmail.com>

--- setup.py
+++ setup.py
@@ -1,27 +1,3 @@
-from setuptools import setup, find_packages
-from os import path
+from setuptools import setup
 
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md')) as f:
-    long_description = f.read()
-
-setup(
-    name='Kairos',
-    url='https://github.com/timelyart/Kairos',
-    packages=find_packages(),
-    license='End-User License Agreement: https://www.eulatemplate.com/live.php?token=zduUYsiBp0PFzzX0UANHUodYVMifuNRp',
-    author='timelyart',
-    version='2.89',
-    author_email='timelyart@protonmail.com',
-    description='Kairos aims to help you save time by automating repetitive tasks on TradingView such as refreshing '
-                'alerts and creating new ones.',
-    long_description=long_description,
-    install_requires=['pyyaml', 'beautifulsoup4', 'urllib3>=1.26,<3', 'selenium', 'configparser', 'pip', 'Pillow',
-                      'requests', 'gspread', 'google-api-python-client', 'oauth2client', 'pymongo', 'dill', 'numpy',
-                      'fastnumbers', 'psutil', 'Cython', 'tqdm', 'soupsieve', 'coloredlogs'],
-    extras_require={
-        'platform_system == "Windows"': [],
-        'platform_system == "Linux"': ['python3-Xlib', 'pyvirtualdisplay'],
-        'platform_system == "Darwin"': ['pyobjc-core', 'pyobjc']
-    },
-)
+setup()
